ba736e82c1446085e85799b7ab64cfc2407aff84
haukurr11 <haukurr11@ru.is>
2013-01-29 04:55:26 +0000

    Text komið

--- js/oopaint.js
+++ js/oopaint.js
@@ -13,10 +13,49 @@ var prev = [];
 var currentBoard = 0;
 
 $("document").ready(function () {
+    $("#text_formatter").hide();
+    $("#texti").css("font-family",$('#fontselector').val() );
     o_CurrentTool = $("#currentShape").val();
     $('#currentShape').change(function () {
+
         o_CurrentTool = $("#currentShape").val();
+        if(o_CurrentTool == "text") {
+        $("#container canvas").css("cursor","text");
+        }
+        else {
+            $("#container canvas").css("cursor","crosshair");
+        }
         p_mouseEvents = new paintMouseEvents();
+    });
+    $('#fontselector').change(function () {
+        $("#texti").css("font-family",$('#fontselector').val() );
+
+    });
+    $("#boldbtn").click( function() {
+        if($("#texti").css("font-weight") !== "bold") {
+        $("#texti").css("font-weight","bold");
+        }
+        else {
+            $("#texti").css("font-weight","normal");
+        }
+    });
+    $("#itbtn").click( function() {
+        if($("#texti").css("font-style") !== "italic") {
+            $("#texti").css("font-style","italic");
+        }
+        else {
+            $("#texti").css("font-style","normal");
+        }
+    });
+    $("#incsize").click( function() {
+        var fontSize = parseInt($("#texti").css("font-size").replace("px",""));
+        fontSize++;
+        $("#texti").css("font-size",fontSize + "px");
+    });
+    $("#decsize").click( function() {
+        var fontSize = parseInt($("#texti").css("font-size").replace("px",""));
+        fontSize--;
+        $("#texti").css("font-size",fontSize + "px");
     });
     //MAINCANVAS
     maincanvas = document.getElementById('imageView');
@@ -47,9 +86,11 @@ $("document").ready(function () {
         onChange: function (hsb, hex, rgb) {
             $('#colorp div').css('backgroundColor', '#' + hex);
             o_currentColor = "#" + hex;
+            $("#texti").css("color",o_currentColor);
         }
     });
     o_currentColor = $(".colorpicker_hex input").val();
+    $('#colorp').ColorPickerSetColor(o_currentColor);
     $('#colorp div').css('backgroundColor', '#' + o_currentColor);
     $("#boards li a").click( function() {
         var num = parseInt($(this).attr("id").substring(1));
@@ -60,7 +101,6 @@ $("document").ready(function () {
 
 function changeBoard(boardnum) {
     currentBoard = boardnum;
-    console.log(boardnum,currentBoard);
     redraw();
 };
 
@@ -197,19 +237,47 @@ var Circle = ShapeBase.extend({
 });
 
 var Text = ShapeBase.extend({
-    constructor: function(x, y, x2, y2, color, lineWidth) {
+    constructor: function(x, y,inputText,fontSize,fontType,fontStyle,fontWeight, color, lineWidth) {
         this.base(x, y, color, lineWidth);
-        this.inputText;
-        this.xEnd = x2;
-        this.yEnd = y2;
+        this.inputText = inputText;
+        this.fontSize = fontSize;
+        this.fontType = fontType;
+        this.color = color.replace("#","");
+        this.fontStyle = fontStyle;
+        this.fontWeight = fontWeight;
+        var fs = parseInt(this.fontSize.replace("px",""));
+        this.xEnd = this.x + (inputText.length*fs)/1.5;
+        console.log(this.xEnd);
+        this.yEnd = this.y+fs;
     },
     draw: function(ctx) {
         ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
         ctx.lineWidth = this.lineWidth;
-        ctx.font = '40px san-serif';
-        var bounds = this.calcbounds()
+        ctx.font = this.fontStyle + " " + this.fontWeight + " " + this.fontSize + " " + this.fontType;
+        console.log(ctx.font);
+        var bounds = this.calcbounds();
         ctx.textBaseline = "top";
-        ctx.strokeText(this.inputText, bounds.x, bounds.y);
+        ctx.fillText(this.inputText,bounds.x,bounds.y);
+    },
+    Edit: function() {
+
+        var container = $("#container");
+        var textelement = $('<input type="text" editing="true" id="texti"/>');
+        textelement.attr("value",this.inputText);
+        textelement.css("top",this.y + "px");
+        textelement.css("left",this.x + "px");
+        textelement.css("font-family",this.fontType);
+        textelement.css("font-size",this.fontSize);
+        textelement.css("font-style",this.fontStyle);
+        textelement.css("font-weight",this.fontWeight);
+        textelement.css("color","#" + this.color);
+        container.append(textelement);
+        $("#text_formatter").show();
+        $('#colorp').ColorPickerSetColor("#" + this.color);
+        $('#colorp').find('div').css('backgroundColor', '#' + this.color);
+        o_currentColor =  this.color;
+        return;
     }
 })
 
@@ -294,6 +362,10 @@ function ev_canvas(ev) {
     }
 };
 
+function get_type(thing){
+    if(thing===null)return "[object Null]"; // special case
+    return Object.prototype.toString.call(thing);
+}
 function paintMouseEvents() {
     var tool = this;
     this.started = false;
@@ -304,12 +376,44 @@ function paintMouseEvents() {
     tool.prevY;
 
     this.mousedown = function (ev) {
+        if( $("#texti").val() ) {
+            var text = $("#texti").val();
+            console.log(text);
+            var fontSize = $("#texti").css("font-size");
+            var fontType= $("#texti").css("font-family");
+            var fontWeight= $("#texti").css("font-weight");
+            var fontStyle= $("#texti").css("font-style");
+            //(x, y,inputText,fontSize,fontType,fontStyle, color, lineWidth)
+            if(!$("#texti").attr("editing")) {
+            var textobject = new Text(tool.prevX,tool.prevY,text,fontSize,fontType,fontStyle,fontWeight, o_currentColor, o_lineWidth);
+            p_drawnShapes[currentBoard].push(textobject);
+            textobject.draw(maincontext);
+            }
+            else {
+                tool.selected.inputText = $("#texti").val();
+                tool.selected.fontType = $("#texti").css("font-family");
+                tool.selected.fontSize = $("#texti").css("font-size");
+                tool.selected.fontWeight = $("#texti").css("font-weight");
+                tool.selected.fontStyle = $("#texti").css("font-style");
+                tool.selected.color = o_currentColor.replace("#","");
+                redraw();
+            }
+            $("#text_formatter").hide();
+            $("#texti").remove();
+            return;
+        }
+        $("#texti").remove();
         $(this).css('cursor', 'crosshair');
         tool.prevX = ev._x;
         tool.prevY = ev._y;
         if (o_CurrentTool === "select") {
             for (var s in p_drawnShapes[currentBoard]) {
                 if (p_drawnShapes[currentBoard][s].isPointInShape(ev._x, ev._y)) {
+                    if(p_drawnShapes[currentBoard][s].inputText) {
+                        p_drawnShapes[currentBoard][s].Edit();
+                        tool.selected = p_drawnShapes[currentBoard][s];
+                        return;
+                    }
                     tool.selected = p_drawnShapes[currentBoard][s];
                     var bounds = p_drawnShapes[currentBoard][s].calcbounds();
                     tempcontext.strokeStyle = "#F4D71D";
@@ -320,17 +424,36 @@ function paintMouseEvents() {
             }
             return;
         }
+        else if (o_CurrentTool === "text") {
+
+            var container = $("#container");
+            var textelement = $('<input type="text" id="texti"/>');
+            textelement.css("top",ev._y + "px");
+            textelement.css("left",ev._x + "px");
+            textelement.css("color","#" + o_currentColor.replace("#",""));
+            container.append(textelement);
+            $("#text_formatter").show();
+            return;
+        }
+        $("#text_formatter").hide();
         tool.started = true;
         tool.x0 = ev._x;
         tool.y0 = ev._y;
-        if (o_CurrentTool === "pencil") {
-            tempcontext.beginPath();
-        }
     };
 
 
     this.mousemove = function (ev) {
+        //$("#draghandle").remove();
         if (!tool.started) {
+            if (o_CurrentTool === "select") {
+                for (var s in p_drawnShapes[currentBoard]) {
+                    if (p_drawnShapes[currentBoard][s].isPointInShape(ev._x, ev._y)) {
+                        if(p_drawnShapes[currentBoard][s].inputText) {
+                            //ADDA DRAGHANDLE
+                        }
+                    }
+                }
+            }
             return;
         }
         var currentTool;
@@ -341,7 +464,7 @@ function paintMouseEvents() {
         } else if (o_CurrentTool === "circle") {
             currentTool = new Circle(tool.prevX,tool.prevY,ev._x,ev._y,o_currentColor,o_lineWidth);
         } else if (o_CurrentTool === "text") {
-            currentTool = new Text(tool.x0, tool.y0, ev._x, ev._y, o_currentColor, o_lineWidth);
+            return;
         }
         else if (o_CurrentTool === "pencil") {
             tool.points.push({
@@ -364,7 +487,7 @@ function paintMouseEvents() {
             tool.prevY = ev._y;
             return;
         } else {
-            alert("ERROR SHAPE NOT FOUND");
+            alert("ERROR TOOL NOT FOUND");
             return;
         }
         tempcontext.clearRect(0, 0, tempcanvas.width, tempcanvas.height);
@@ -374,6 +497,7 @@ function paintMouseEvents() {
 
     // This is called when you release the mouse button.
     this.mouseup = function (ev) {
+        $("#texti").focus();
         if (o_CurrentTool === "select") {
             tempcontext.clearRect(0, 0, tempcanvas.width, tempcanvas.height);
             tool.started = false;
